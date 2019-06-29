e38a021cf21d6ccd351a47f44d37510823d45538
liuuzn <liuu.zn@gmail.com>
2019-06-29 17:27:19 -0400

    w2

--- w2/Permutation.java
+++ w2/Permutation.java
@@ -4,8 +4,25 @@
  *  Description:
  **************************************************************************** */
 
-public class Permutation {
-    public static void main(String[] args) {
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
+public class Permutation
+{
+    public static void main(String[] args)
+    {
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+
+        int k = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < k; ++i)
+        {
+             rq.enqueue(StdIn.readString());
+        }
+
+        for (String s : rq)
+        {
+            StdOut.println(s);
+        }
     }
 }
